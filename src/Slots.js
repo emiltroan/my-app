541ff0da06789143d5c9d5ea6803318a298c3bbd
Tnessemo <tnessemo05@gmail.com>
2023-03-17 10:47:00 +0100

    hei hei

--- src/Slots.js
+++ src/Slots.js
@@ -3,13 +3,37 @@ import Card from 'react-bootstrap/Card';
 
 export function Slots(){
 
-    return (
+  
+  const allSlots = ["Diamand", "Gull", "TABULATOR", "Emil", "Worgen"];
+
+
+  let slot1 = useState("diamant");
+  let slot2 = useState("Stein");
+  let slot3 = useState("Gull");
+  
+  
+  function spin(){
+    const picker = Math.floor(Math.random() *6);
+  }
+
+  slot1 = allSlots[number(picker)]
+  console.log(slot1)
+  slot2 = allSlots[number(picker)]
+  console.log(slot2)
+  slot3 = allSlots[number(picker)]
+  console.log(slot3)
+
+  spin();
+
+  
+  return (
 <>
 
 
   <div class="Cards">
     <Card>
       <Card.Img variant="top" src="holder.js/100px180" height="400px" width="130px" />
+        <p>{slot1}</p>
       <Card.Img variant="top" src="holder.js/100px180" height="400px" width="130px" />
       <Card.Img variant="top" src="holder.js/100px180" height="400px" width="130px" />
     </Card>
