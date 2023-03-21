fd3f3f697d2673f802d98920b3dd7b2d6de32a86
emil.rognan.skjetne <emil.rognan.skjetne@nte.no>
2023-03-21 12:23:29 +0100

    hei

--- src/Slots.js
+++ src/Slots.js
@@ -1,39 +1,33 @@
-import React from "react"
+import React, {useState} from "react"
 import Card from 'react-bootstrap/Card';
+
 
 export function Slots(){
 
     const allSlots = ["Diamant", "Gull", "TABULATOR", "Emil", "Worgen"];
 
-    let slot1 = "Diamant";
-    let slot2 = "Stein";
-    let slot3 = "Gull";
+    const [slot1, setSlot1] = useState("Diamant");
+    const [slot2, setSlot2] = useState("Diamant");
+    const [slot3, setSlot3] = useState("Diamant");
 
 
-    let picker = Math.floor(Math.random() * 5).toFixed();
+    let picker = ()=>Math.floor(Math.random() * 5).toFixed();
 
     function Spin(){
-
-        slot1 = allSlots[Number(picker)];
-        console.log(slot1);
-        slot2 = allSlots[Number(picker)]
-        console.log(slot2);
-        slot3 = allSlots[Number(picker)]
-        console.log(slot3);
-
+        setSlot1(allSlots[picker()])
+        setSlot2(allSlots[picker()])
+        setSlot3(allSlots[picker()])
     }
 
-    Spin();
-
     return (
-<>
-  <div class="Cards">
-    <Card>
-      <Card.Img variant="top" src="holder.js/100px180" height="400px" width="130px" />
-      <Card.Img variant="top" src="holder.js/100px180" height="400px" width="130px" />
-      <Card.Img variant="top" src="holder.js/100px180" height="400px" width="130px" />
-    </Card>
-  </div>
-</>
+        <>
+          <div class="Cards">
+            <Card>
+              <Card.Img variant="top" src="holder.js/100px180" height="400px" width="130px" />
+              <Card.Img variant="top" src="holder.js/100px180" height="400px" width="130px" />
+              <Card.Img variant="top" src="holder.js/100px180" height="400px" width="130px" />
+            </Card>
+          </div>
+        </>
     )
 }
